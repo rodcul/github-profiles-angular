e65479ce675a40b308064ee90716d3a62b45d469
Rodney Cullen <rodcul@gmail.com>
2015-06-09 19:05:39 +0100

    Data binding (tests passing)

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -25,6 +25,8 @@ describe('GitUserSearchController', function() {
 		}];
 
 		it('displays search results', function() {
+			ctrl.searchTerm = 'hello';
+			ctrl.doSearch();
 			expect(ctrl.searchResult.items).toEqual(items);
 		});
 	});
